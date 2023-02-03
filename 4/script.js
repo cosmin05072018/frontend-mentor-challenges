9056648775aa16598a43190b9796f48d8ba99111
Morari Cosmin <cosminmorari99@yahoo.com>
2023-02-03 15:59:40 +0200

    continue work...

--- 4/script.js
+++ 4/script.js
@@ -2,9 +2,15 @@ const makeReadBtn = document.querySelector('.markRead');
 const checkRead = document.querySelectorAll('.notRead');
 const redCircle = document.querySelectorAll('.newNotificationRed');
 const numberMessagesUnread = document.getElementById('number');
+const valueInput = document.getElementById('valueInput');
 
 
+let maxNotifications = 3;
+numberMessagesUnread.innerHTML = maxNotifications;
 
+console.log(valueInput)
+
+console.log(numberMessagesUnread)
 makeReadBtn.addEventListener('click', () => {
     redCircle.forEach((item) => {
         item.style.display = 'none';
@@ -12,5 +18,18 @@ makeReadBtn.addEventListener('click', () => {
     checkRead.forEach((item) => {
         item.classList.remove('notRead');
     })
-    numberMessagesUnread.innerText = 0;
+    numberMessagesUnread.innerHTML = 0;
+    makeReadBtn.style.display = 'none';
+})
+
+
+
+checkRead.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.remove('notRead');
+        numberMessagesUnread.innerHTML -= 1;
+        if (numberMessagesUnread < 1) {
+            numberMessagesUnread.innerText = 0;
+        }
+    })
 })
