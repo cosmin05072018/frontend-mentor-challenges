2029bbe2a6b624a8ef7432d2ee92f5fc7aa0db8b
Morari Cosmin <cosminmorari99@yahoo.com>
2023-07-05 13:22:24 +0300

    push

--- 11/todo-app-main/saveMarkElement.js
+++ 11/todo-app-main/saveMarkElement.js
@@ -1,15 +1,15 @@
 let statusElements = document.querySelectorAll(".status");
-
-
-statusElements.forEach((element)=>{
+function displayCheck(element) {
   if (element.classList.contains("markStatus")) {
     element.innerHTML = `<img src="./icon-check.svg" alt="iconCheck" />`;
   } else {
     element.innerHTML = "";
   }
-})
+}
 
 function markElement(mark) {
+  mark.classList.toggle("markStatus");
+  mark.nextElementSibling.classList.toggle("markItemList");
   let arr;
   let iconCheckFromLocalStorage = localStorage.getItem("iconCheckId");
   if (iconCheckFromLocalStorage) {
@@ -24,24 +24,11 @@ function markElement(mark) {
   } else {
     arr.push(idMark);
   }
-  for (let i = 0; i < statusElements.length; i++) {
-    let element = statusElements[i];
-    element.classList.toggle("markStatus");
-    element.nextElementSibling.classList.toggle("markItemList");
-    // if (element.classList.contains("markStatus")) {
-    //   element.innerHTML = `<img src="./icon-check.svg" alt="iconCheck" />`;
-    // } else {
-    //   element.innerHTML = "";
-    // }
-  }
-
   localStorage.setItem("iconCheckId", JSON.stringify(arr));
-
-  // statusElements.forEach((element) => {
-  //   if (arr.includes(element.id)) {
-  //     element.classList.add("markStatus");
-  //   } else {
-  //     element.classList.remove("markStatus");
-  //   }
-  // });
+  displayCheck(mark); //actualizam in timp real
 }
+
+
+statusElements.forEach((element) => {
+  displayCheck(element);
+});
